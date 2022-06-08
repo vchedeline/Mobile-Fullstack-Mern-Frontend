280058f4f18c3d53213f1e1e9cafbfa6ba3442c7
chedeline <vchedeline@gmail.com>
2022-06-08 13:17:25 -0400

    Css

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import "./App.css";
 import Nav from "./components/Nav";
 import Main from "./components/Main";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
+      <header className="navbar sticky-top">
+        <Header />
+      </header>
       <Main />
       <footer className="navbar sticky-bottom">
-        <div className="container-fluid">
+        <div className="container-fluid nav">
           <Nav />
         </div>
       </footer>
---
+++ src/components/Header.jsx
@@ -0,0 +1,3 @@
+export default function Header() {
+  return <div>Chedou's Fast Food</div>;
+}
